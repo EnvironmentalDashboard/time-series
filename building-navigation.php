fdcbd8a31794cce7b6afb4492e1988b94f12c2bd
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-02-04 11:09:39 -0500

    first commit

--- building-navigation.php
+++ building-navigation.php
@@ -9,6 +9,7 @@ ini_set('display_errors', 'On');
   <meta charset="UTF-8">
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
   <link rel="stylesheet" href="css/bootstrap.grid.css">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"> -->
   <title>Building Navigation</title>
   <style>
     body {
@@ -19,14 +20,15 @@ ini_set('display_errors', 'On');
     }
     .card {
       height: 450px;
-      width: calc(33.333% - 20px);
+      /*width: calc(33.333% - 10px);*/
+      width: 100%;
       margin-bottom: 10px;
       background: #fff;
       padding: 10px;
-      display: inline-block;
-      margin-right: 10px;
-      float: left;
-      position: relative;
+      /*display: inline-block;*/
+      /*margin-right: 10px;*/
+      /*float: left;*/
+      /*position: relative;*/
     }
     .card img {
       width: 100%;
@@ -78,11 +80,22 @@ ini_set('display_errors', 'On');
 <body>
   <div class="container">
     <div class="row">
-      <div class="col-sm-9">
+      <div class="col-sm-3 col-sm-push-9">
+        <input type="text" id="search" placeholder="Search">
+        <ul>
+          <?php foreach($db->query('SELECT DISTINCT building_type FROM buildings WHERE
+          custom_img IS NOT NULL AND id IN (SELECT building_id FROM meters WHERE num_using > 0 OR for_orb > 0) ORDER BY building_type ASC') as $building) { ?>
+          <li class="filter-btn" data-buildingtype="<?php echo $building['building_type']; ?>"><?php echo $building['building_type']; ?></li>
+          <?php } ?>
+        </ul>
+      </div>
+      <div class="col-sm-9 col-sm-pull-3">
+        <div class="row">
         <?php foreach($db->query('SELECT id, name, building_type, custom_img FROM buildings WHERE
                                 custom_img IS NOT NULL AND id IN
                                 (SELECT building_id FROM meters WHERE num_using > 0 OR for_orb > 0)
                                 ORDER BY building_type ASC, name') as $building) { ?>
+        <div class="col-sm-4">
         <div class="card" data-title="<?php echo $building['name'] ?>" data-buildingtype="<?php echo $building['building_type'] ?>">
           <img src="<?php echo $building['custom_img'] ?>" alt="<?php echo $building['name'] ?>">
           <h1><?php echo $building['name'] ?></h1>
@@ -98,16 +111,9 @@ ini_set('display_errors', 'On');
             } ?>
           </p>
         </div>
+        </div>
         <?php } ?>
-      </div>
-      <div class="col-sm-3">
-        <input type="text" id="search" placeholder="Search">
-        <ul>
-          <?php foreach($db->query('SELECT DISTINCT building_type FROM buildings WHERE
-          custom_img IS NOT NULL AND id IN (SELECT building_id FROM meters WHERE num_using > 0 OR for_orb > 0) ORDER BY building_type ASC') as $building) { ?>
-          <li class="filter-btn" data-buildingtype="<?php echo $building['building_type']; ?>"><?php echo $building['building_type']; ?></li>
-          <?php } ?>
-        </ul>
+        </div>
       </div>
     </div>
   </div>
